23e0cd24f7b48f32ce69ee8faf459531f3e11343
channicq <clement.hannicq@revevol.eu>
2013-07-24 13:51:39 +0200

    Revert "Added example"
    This reverts commit b0398f1784710195ffd6b611ce97a61571d76903.

--- exemple/date.sql
+++
@@ -1,7 +0,0 @@
-(@INC(mesure:@VAR0$:@VAR1$:IT)),
-(@INC(mesure:@VAR0$:@VAR1$:TURNOVEREUR)),
-(@INC(mesure:@VAR0$:@VAR1$:INVQTY)),
-(@INC(mesure:@VAR0$:@VAR1$:ITVIALNB)),
-(@INC(mesure:@VAR0$:@VAR1$:MTVIALNB)),
-(@INC(mesure:@VAR0$:@VAR1$:PACKNB)),
-(@INC(mesure:@VAR0$:@VAR1$:PACKMT))
--- exemple/templateExecute.py
+++
@@ -1,6 +0,0 @@
-import queryt
-import time
-import os
-path = os.path.dirname(os.path.abspath(__file__))
-queryt.queryt(path, 'template.sql')
-time.sleep(10)
