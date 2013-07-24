2a0603b550cae3a9696bd1f6ccbff0db2db09293
channicq <clement.hannicq@revevol.eu>
2013-07-24 10:02:44 +0200

    Added range
    Added the @RNG(int_min:int_max) function, compatible to be used as
    agrument of an @INC(temp:args) function. (ex: @INC(temp:@RNG(1:3))
    
    Use of @VARn$ as argument of @RNG is also supported

--- queryt.py
+++ queryt.py
@@ -6,8 +6,6 @@ def includeTemplate(path, template_path, file, args = []):
     print(temp)
     c_include = "@INC("
     c_range = "@RNG("
-
-    
     template = open(path+"\\"+template_path, "r")
     n_args = len(args)
     for line in template:
@@ -16,28 +14,51 @@ def includeTemplate(path, template_path, file, args = []):
             line = line.replace("@VAR"+str(i)+"$", args[i])
             print("Replace call:'"+"@VAR"+str(i)+"$"+"', "+args[i])
         print("(Repl)"+line)
-        index = line.rfind(c_include)
-        if index != -1:
-            index_end = -1
-            for i in range(index+len(c_include), len(line)):
-                if line[i] == ")":
-                    if index_end == -1:
-                        index_end = i
-            include_args = line[index+5:index_end]
-            print("  Include found : include("+include_args+")"+str(index))
-            include_seq = include_args.split(':')
-            n_include = len(include_seq)
-            to_erase = line[index:index_end+1]
-            print("Replace call:'"+to_erase+"', void")
-            print("(Incl)"+line)
-            to_erase = line[index:index_end+1]
-            line = line.split(to_erase)
-            file.write(line[0])
-            includeTemplate(path, include_seq[0]+".sql", file, include_seq[1:])
-            file.write(line[1])
-            line = ""
+        line = parseRange(line, file, path, c_range, c_include)
+        line = parseInclude(line, file, path, c_include)
         file.write(line)
     template.close
+
+def parseRange(line, file, path, c_range, c_include):
+    index = line.rfind(c_range)
+    if index != -1:
+        index_end = -1
+        for i in range(index+len(c_include), len(line)):
+            if line[i] == ")":
+                if index_end == -1:
+                    index_end = i
+        include_args = line[index+5:index_end]
+        print("  Range found : range("+include_args+")")
+        include_seq = include_args.split(':')
+        to_erase = line[index:index_end+1]
+        for i in range(int(include_seq[0]), int(include_seq[1])+1):
+            newline = line.replace(to_erase, str(i))
+            newline = parseInclude(newline, file, path, c_include)
+            file.write(newline)
+        line = ""
+    return line
+
+def parseInclude(line, file, path, c_include):
+    index = line.rfind(c_include)
+    if index != -1:
+        index_end = -1
+        for i in range(index+len(c_include), len(line)):
+            if line[i] == ")":
+                if index_end == -1:
+                    index_end = i
+        include_args = line[index+5:index_end]
+        print("  Include found : include("+include_args+")")
+        include_seq = include_args.split(':')
+        n_include = len(include_seq)
+        to_erase = line[index:index_end+1]
+        print("Replace call:'"+to_erase+"', void")
+        print("(Incl)"+line)
+        line = line.split(to_erase)
+        file.write(line[0])
+        includeTemplate(path, include_seq[0]+".sql", file, include_seq[1:])
+        file.write(line[1])
+        line=""
+    return line
 
 def queryt(path, template):
     file = open(path+"\\query.sql.txt", "w")
